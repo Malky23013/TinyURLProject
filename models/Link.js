ae59a8e984f1fe7f673143ff0a1674c8fea8a828
Malky23013 <148490530+Malky23013@users.noreply.github.com>
2024-06-04 23:32:44 +0300

    Update Link.js

--- models/Link.js
+++ models/Link.js
@@ -1,27 +1,4 @@
-// const mongoose = require('mongoose');
-
-// const clickSchema = new mongoose.Schema({
-//     insertedAt: { type: Date, default: Date.now },
-//     ipAddress: String,
-//     targetParamValue: String
-// });
-
-// const linkSchema = new mongoose.Schema({
-//     originalUrl: String,
-//     clicks: [clickSchema],
-//     targetParamName: { type: String, default: 't' },
-//     targetValues: [{
-//         name: String,
-//         value: String
-//     }]
-// });
-
-// const Link = mongoose.model('Link', linkSchema);
-
-// module.exports = Link;
-// export default Link;
 import mongoose from 'mongoose';
-
 const clickSchema = new mongoose.Schema({
   insertedAt: {
     type: Date,
